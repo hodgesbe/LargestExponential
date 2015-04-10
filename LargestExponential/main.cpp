07f8be594dccf69fb7f9402cab621665308dfabe
Brett Hodges <bhodges87@gmail.com>
2015-04-09 22:23:03 -0400

    Initial Commit

---
+++ LargestExponential/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LargestExponential
+//
+//  Created by Brett Hodges on 4/9/15.
+//  Copyright (c) 2015 Brett Hodges. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
